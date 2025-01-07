4d21c6de9c88b1a24fa23c27702e29cfffc74848
Ken Morishita <mokemokechicken@gmail.com>
2025-01-07 07:07:38 +0000

    VSCode設定ファイルのインデントを修正し、biomeの設定を追加。package.jsonにフォーマットとリントのスクリプトを追加。

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { HttpFunction } from "@google-cloud/functions-framework";
 
 export const helloGET: HttpFunction = (req, res) => {
-	res.send("Hello, World!");
+  res.send("Hello, World!");
 };
-
